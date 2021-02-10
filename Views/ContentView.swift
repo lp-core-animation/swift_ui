67cea026e851005e44cea5895b449a9face6a9f3
Lucas <lucaspedrazoli@gmail.com>
2021-02-10 17:34:51 -0300

    add animation on thumb

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -10,13 +10,21 @@ struct ContentView : View {
       ZStack {
         HStack {
           TourTitle(title: "Savanna Trek", caption: "15 mile drive followed by an hour long trek")
-            .offset(x: 0, y: -15)
+            .offset(x: self.zoomed ? 500 : 0, y: -15)
+            .animation(.default)
             .padding(.leading, 30)
           
           Spacer()
         }
         
         Image("thumb")
+          .overlay(
+            Circle()
+              .fill(self.zoomed ? Color.clear : Color(red: 1.0, green: 1.0, blue: 1.0, opacity: 0.4))
+              .scaleEffect(0.8)
+            )
+          .saturation(self.zoomed ? 1 : 0)
+          .clipShape(RoundedRectangle(cornerRadius: self.zoomed ? 40 : 500 ))
           .position(x: self.zoomed ? 220 : 600, y: 50)
           .scaleEffect(self.zoomed ? 1.33 : 0.33)
           .shadow(radius: 10)
